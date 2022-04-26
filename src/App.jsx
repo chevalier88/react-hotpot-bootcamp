f8971b4fc95f4c6a236347c26420f1a4dc5e23f3
Graham <graham.lim88@gmail.com>
2022-04-26 16:50:28 +0800

    show form function debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+function BillForm() {
+  // Control the form input value using a state variable name.
+  const [bills, setBills] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // prevents refreshing?
+    console.log('asdsa');
+    console.log(event.target.Bill.value);
+    const newBills = [...bills];
+    newBills.push(event.target.Bill.value);
+    setBills(newBills);
+  };
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Bill</Form.Label>
+          <Form.Control placeholder="Bill input" name="Bill" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+
+      </Form>
+      <br />
+      <br />
+      Bills So Far:
+      {' '}
+      <ul>
+        {bills.map((bill) => (
+          <li key={bill.toString()}>
+            {bill}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Example() {
+  const [showForm, setShowForm] = useState(false);
+
+  const showFormHandler = () => {
+    setShowForm(true);
+  };
+
+  return (
+    <div>
+      <form>
+        <button onClick={showFormHandler}>Create New Bill</button>
+      </form>
+
+      {showForm && (
+        <BillForm />
+      )}
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <div>
-      This is App.jsx
+      {/* <BillForm /> */}
+      <Example />
     </div>
   );
 }
